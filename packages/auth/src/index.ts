403d29db95637d509170f651379e55d82586edfb
aaapwn <armmykung2547@gmail.com>
2026-03-10 19:39:36 +0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,23 @@
+import prisma from "@ez-jira-log/db";
+import { env } from "@ez-jira-log/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
